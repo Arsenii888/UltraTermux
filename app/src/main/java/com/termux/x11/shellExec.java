d7a062a459f85bf67ea3f4223b857c9b754e42d2
Arsenii888 <143905189+Arsenii888@users.noreply.github.com>
2024-09-01 21:03:22 +0300

    Update shellExec.java

--- app/src/main/java/com/termux/x11/shellExec.java
+++ app/src/main/java/com/termux/x11/shellExec.java
@@ -1,3 +1,12 @@
+package com.u063.horizontema.shell;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
 public class shellExec {
     public void runShell(final String command) {
 
